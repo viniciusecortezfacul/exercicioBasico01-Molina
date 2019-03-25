411d7551b0d62a64270e1c5ff6bee727841368a4
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-03-25 16:21:21 -0300

    Criado método MaiorNumero.maiorNumero() softiscado

---
+++ Revisao_Logica/src/revisao_logica/NumeroDecrescente.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package revisao_logica;
+
+import java.util.List;
+
+/**
+ *
+ * @author federal
+ */
+public class NumeroDecrescente {
+    
+    public static List<Number> numeroDecrescente(Comparable n){
+        
+        for (int i = 0; n.compareTo(i); i++) {
+            Object object = arr[i];
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
--- Revisao_Logica/src/revisao_logica/Revisao_Logica.java
+++ Revisao_Logica/src/revisao_logica/Revisao_Logica.java
@@ -15,7 +15,7 @@ public class Revisao_Logica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(MaiorNumero.maiorNumero(2,3,50,4));
+        Comparable k = MaiorNumero.maiorNumero(2,3.5,50,4));
     }
     
 }
